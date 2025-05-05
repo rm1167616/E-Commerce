5d3e881b1d1050399ba5b8cba430e2107febdfd4
toqa osama <82851804+toqaosama@users.noreply.github.com>
2025-05-05 15:29:46 +0300

    tabel form

--- client/src/component/AdminCycle/DashBoard/Dashboard.js
+++ client/src/component/AdminCycle/DashBoard/Dashboard.js
@@ -1,11 +1,18 @@
+import { Container } from 'react-bootstrap';
 import FilterBar from '../FilterBar/FilterBar';
+import Tables from '../tables/Table';
 
 function Dashboard() {
   return (
-    <>
+    <Container>
+    <div className="flex flex-row">
       <FilterBar />
-      
-    </>
+    </div>
+    <div className="flex flex-row"
+    style={{marginTop:'5%'}}>
+      <Tables />
+    </div>
+    </Container>
   );
 }
 
