c9600de1d04d5a479d6173564fdeab07227407e8
dalia-hegazy <daliahegazy346@gmail.com>
2025-05-09 15:05:05 +0300

    footer handle and cards

--- client/src/component/Shared/Home.js
+++ client/src/component/Shared/Home.js
@@ -1,7 +1,26 @@
-// pages/Home.js
-function Home() {
-    return <h1>Home Page</h1>;
-  }
+
+  import React, { Component } from 'react'
+  import { Container } from 'react-bootstrap';
+
+import CardTemplate2 from '../Template1/Cards/Sections/CardTemplate2';
+
+
   
-  export default Home; // ✅ default export
+  const Home = () => {
+    
+      return (
+       <Container>
+    <div className="flex flex-row"
+   >
+    
+     <CardTemplate2/>
      
+
+    </div>
+    </Container>
+      );
+    
+  };
+  
+  export default Home;
+  
